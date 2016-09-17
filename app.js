4e0d44c272ee39f632befeaae9ae86961d23c120
Hannes Filler <hfiller@v1020-wn-97-3.campus-dynamic.uwaterloo.ca>
2016-09-17 05:51:12 -0400

    updated app

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // load static folder
 var express = require("express");
+var http = require('http');
 var app = express();
 // LOL @ express server implementations
-app.use("/",express.static(path.join(__dirname, "public")));
-app.get("*",function(){
+app.use("/",express.static(__dirname+"/public"));
+app.get("/",function(req, res){
 	res.sendfile(__dirname+'/public/index.html');
-})
+});
+
+var PORT = "8080";
+var ADDRESS = "0.0.0.0";
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(PORT,ADDRESS);
